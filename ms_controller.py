6134c77d72237c2633ef56aaf863f1fc5ac1bc88
sschachenhofer <sschachenhofer@student.tgm.ac.at>
2020-01-11 13:35:34 +0100

    Basic game working

--- ms_controller.py
+++ ms_controller.py
@@ -2,10 +2,24 @@ from PyQt5.QtCore import QSignalMapper, Qt
 from PyQt5.QtWidgets import QDialog, QGridLayout, QLabel, QMainWindow, QPushButton, QVBoxLayout
 
 from ms_window import Ui_MainWindow
-from ms_model import MinesweeperModel, MineFound, AlreadyUncoveredError
+from ms_model import *
 
 
 class MinesweeperController(QMainWindow):
+
+    RED_BG = "background-color: red; color: black;"
+    BLUE_BG = "background-color: blue; color: white;"
+    GREEN_BG = "background-color: green; color: black;"
+    FONT = "font-weight: 1000; font-size: 2em;"
+    NO_BORDER = "border: none;"
+
+    MINE_MISSED_STYLE = f"* {{ {RED_BG} {FONT} {NO_BORDER} }}"
+    MINE_FOUND_STYLE = f"* {{ {GREEN_BG} {FONT} {NO_BORDER} }}"
+
+    MINE_TAGGED_STYLE = f"* {{ {RED_BG} {FONT} {NO_BORDER} }}"
+    MINE_POSSIBLE_STYLE = f"* {{ {BLUE_BG} {FONT} {NO_BORDER} }}"
+    UNCOVERED_STYLE = f"* {{ {FONT} }}"
+
     def __init__(self, columns: int = 9, rows: int = 9, mines: int = 10):
         super().__init__(parent=None)
 
@@ -27,33 +41,43 @@ class MinesweeperController(QMainWindow):
         self.mapper = QSignalMapper(self.view.centralwidget)
         self.mapper.mapped.connect(self.button_clicked)
 
-        # Add the buttons to the GUI
-        grid = QGridLayout()
-        self.view.main_layout.addLayout(grid)
-        grid.setSpacing(5)
+        # Initialize another QSignalMapper to enable mapping of right-clicks
+        self.rc_mapper = QSignalMapper(self.view.centralwidget)
+        self.rc_mapper.mapped.connect(self.button_right_clicked)
+
+        # Create a QGridLayout for the buttons
+        self.grid = QGridLayout()
+        self.view.main_layout.addLayout(self.grid)
+        self.grid.setSpacing(5)
 
         # This is the only way to keep a reference to all buttons on the board, since buttons are "re-parented" to the
         # main window after being added to a layout
         self.buttons = []
 
+        # Add the buttons to the GUI
         for n in range(self.columns * self.rows):
             button = QPushButton()
             button.setFixedSize(40, 40)
-            self.mapper.setMapping(button, n)
-            button.clicked.connect(self.mapper.map)
 
-            grid.addWidget(button, int(n / self.columns), n % self.columns, alignment=Qt.AlignCenter)
+            # Connect the button signals
+            self.mapper.setMapping(button, n)
+            self.rc_mapper.setMapping(button, n)
+            button.clicked.connect(self.mapper.map)
+            button.customContextMenuRequested.connect(self.rc_mapper.map)
+            button.setContextMenuPolicy(Qt.CustomContextMenu)
+
+            self.grid.addWidget(button, int(n / self.columns), n % self.columns, alignment=Qt.AlignCenter)
             self.buttons.append(button)
 
         # Resize the rows
-        for i in range(grid.rowCount()):
-            grid.setRowMinimumHeight(i, 30)
+        for i in range(self.grid.rowCount()):
+            self.grid.setRowMinimumHeight(i, 40)
 
         # Resize the columns
-        for i in range(grid.columnCount()):
-            grid.setColumnMinimumWidth(i, 30)
+        for i in range(self.grid.columnCount()):
+            self.grid.setColumnMinimumWidth(i, 40)
 
-        grid.setSizeConstraint(QGridLayout.SetFixedSize)
+        self.grid.setSizeConstraint(QGridLayout.SetFixedSize)
 
         # For some reason, it really is that complicated to resize the window appropiately...
         window_width = self.view.main_layout.sizeHint().width() + 18
@@ -73,11 +97,45 @@ class MinesweeperController(QMainWindow):
 
         try:
             n = self.model.uncover(x, y)
+            button = self.grid.itemAtPosition(x, y).widget()
+            label = QLabel(str(n))
+            label.setStyleSheet(MinesweeperController.UNCOVERED_STYLE)
+            self.grid.replaceWidget(button, label, options=Qt.FindChildrenRecursively)
+
+            # Delete the button
+            button.deleteLater()
+            self.buttons.remove(button)
 
         except AlreadyUncoveredError:
             pass
+        except FieldTaggedError:
+            self.view.statusbar.showMessage("You need to untag the field before you can open it")
         except MineFound:
             self.__lose()
+
+    def button_right_clicked(self, position):
+        x = int(position / self.columns)
+        y = position % self.columns
+
+        if not self.game_running:
+            self.view.statusbar.showMessage("You need to restart the game!", 5000)
+
+        try:
+            button = self.grid.itemAtPosition(x, y).widget()
+
+            new_state = self.model.switch_tagging(x, y)
+            if new_state == Field.COVERED:
+                button.setStyleSheet("")
+                button.setText("")
+            elif new_state == Field.MINE_TAGGED:
+                button.setStyleSheet(MinesweeperController.MINE_TAGGED_STYLE)
+                button.setText("!")
+            elif new_state == Field.MINE_POSSIBLE:
+                button.setStyleSheet(MinesweeperController.MINE_POSSIBLE_STYLE)
+                button.setText("?")
+
+        except AlreadyUncoveredError:
+            pass
 
     def __lose(self):
         # End the game
@@ -86,6 +144,16 @@ class MinesweeperController(QMainWindow):
         # Disable all buttons
         for button in self.buttons:
             button.setEnabled(False)
+
+        # Show the mine positions:
+        for x, y in self.model.mines:
+            widget = self.grid.itemAtPosition(x, y).widget()
+            widget.setText("\u2715")
+
+            if self.model.field_state(x, y) == Field.COVERED:
+                widget.setStyleSheet(MinesweeperController.MINE_MISSED_STYLE)
+            else:
+                widget.setStyleSheet(MinesweeperController.MINE_FOUND_STYLE)
 
         # Display a message - TODO: Open a dialog instead
         self.view.statusbar.showMessage("You lost :(", 5000)
--- ms_model.py
+++ ms_model.py
@@ -1,38 +1,89 @@
+import enum
 import random
 
 
-class MineFound(Exception):
-    pass
+class Field:
+
+    COVERED = 0
+    MINE_TAGGED = 1
+    MINE_POSSIBLE = 2
+    UNCOVERED = 3
+
+    def __init__(self):
+        self.mine = False
+        self.state = Field.COVERED
+
+    def switch_tagging(self):
+        if self.state == Field.UNCOVERED:
+            raise AlreadyUncoveredError
+        else:
+            self.state = (self.state + 1) % 3
+            return self.state
 
 
 class AlreadyUncoveredError(Exception):
     pass
 
 
+class FieldTaggedError(Exception):
+    pass
+
+
+class MineFound(Exception):
+    pass
+
+
 class MinesweeperModel:
-    def __init__(self, width: int = 9, height: int = 9, mines: int = 10):
+    def __init__(self, width: int = 9, height: int = 9, n_mines: int = 10):
         self.width = width
         self.height = height
-        self.mines = mines
+        self.n_mines = n_mines
+        self.mines = []
 
-        if self.mines > self.width * self.height:
+        if self.n_mines > self.width * self.height:
             raise ValueError("The number of mines can't be higher than the number of fields on the board")
 
         # Matrix for storing mines
-        # The first value specifies whether there is a mine at the field, the second value specifies whether the field
-        # has been uncovered.
-        self.board = [[(False, False) for i in range(self.height)] for j in range(self.width)]
+        # The first value specifies whether there is a mine at the field, the second value specifies the status of the
+        # field: covered, tagged (exclamation mark), in question (question mark) or uncovered
+        self.__board = [[Field() for i in range(self.height)] for j in range(self.width)]
 
         # Hide the specified number of mines on the board
-        for i in range(mines):
+        for i in range(n_mines):
             while True:
                 # Repeat until a spot on the board is found where the mine can be placed
                 x = random.randrange(0, self.width)
                 y = random.randrange(0, self.height)
 
-                if not self.board[x][y][0]:
-                    self.board[x][y] = (True, False)
+                if not self.__board[x][y].mine:
+                    self.__board[x][y].mine = True
+                    self.mines.append((x, y))
                     break
+
+    def field_state(self, x, y):
+        return self.__board[x][y].state
+
+    def switch_tagging(self, x, y):
+        return self.__board[x][y].switch_tagging()
+
+    def uncover(self, x: int, y: int) -> int:
+        if x < 0 or x >= self.width:
+            raise ValueError("Illegal value for x")
+        if y < 0 or y >= self.height:
+            raise ValueError("Illegal value for y")
+
+        if self.__board[x][y].state == Field.UNCOVERED:
+            # The field has been uncovered already
+            raise AlreadyUncoveredError
+        elif self.__board[x][y].state == Field.MINE_TAGGED or self.__board[x][y].state == Field.MINE_POSSIBLE:
+            # The field has been tagged - it needs to be untagged before the player can uncover it
+            raise FieldTaggedError
+        elif self.__board[x][y].mine:
+            # There is a mine at the field
+            raise MineFound
+        else:
+            # Calculate the number of mines surrounding the field
+            return self.__mines_around(x, y)
 
     def __mines_around(self, x, y) -> int:
         if x < 0 or x >= self.width:
@@ -44,23 +95,7 @@ class MinesweeperModel:
 
         for temp_x in range(max(0, x - 1), min(self.width, x + 2)):
             for temp_y in range(max(0, y - 1), min(self.height, y + 2)):
-                if self.board[temp_x][temp_y]:
+                if self.__board[temp_x][temp_y].mine:
                     n += 1
 
         return n
-
-    def uncover(self, x: int, y: int) -> int:
-        if x < 0 or x >= self.width:
-            raise ValueError("Illegal value for x")
-        if y < 0 or y >= self.height:
-            raise ValueError("Illegal value for y")
-
-        if self.board[x][y][1]:
-            # The field has been uncovered already
-            raise AlreadyUncoveredError
-        elif self.board[x][y][0]:
-            # There is a mine at the field
-            raise MineFound
-        else:
-            # Calculate the number of mines surrounding the field
-            return self.__mines_around(x, y)
